528c0bd885afbf2dbf064b2c6cae4e16051c2983
Admin <gohelmihir7031@gmail.com>
2026-02-27 12:20:09 +0530

    App Icon Change

--- app/src/main/java/com/example/gharsetu/ui/authentication/OtpVerificationScreenActivity.kt
+++ app/src/main/java/com/example/gharsetu/ui/authentication/OtpVerificationScreenActivity.kt
@@ -6,16 +6,42 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.gharsetu.R
+import com.example.gharsetu.databinding.ActivityLoginScreenBinding
+import com.example.gharsetu.databinding.ActivityOtpVerificationScreenBinding
 
 class OtpVerificationScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOtpVerificationScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_otp_verification_screen)
+
+        binding = ActivityOtpVerificationScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        init()
+    }
+
+    private fun init(){
+
+        setonClickListeners()
+
+    }
+
+    private fun setonClickListeners() {
+
+        binding.ivBackArrow.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
     }
 }
+
+
